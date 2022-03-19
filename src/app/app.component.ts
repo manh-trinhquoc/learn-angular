0e74ada6bfd8337a70198f51abfbaed51b48d807
manhtq <manhtq@sunshinegroup.vn>
2022-03-19 09:43:53 +0700

    sp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello angular 10';
+  title = 'hello angular 10 initial title';
   description = "heelo world";
   today = new Date();
   hero = {
@@ -17,5 +18,41 @@ export class AppComponent {
     planet: 'Earth',
     color: 'cyan'
   };
+
+  private setTitle = () => {
+    // this.title = 'Hello Angular 10 setTittle';
+    const timestamp = new Date().getMilliseconds();
+    this.title = `Hello Angular 10 (${timestamp})`;
+   
+  }
+
+  private changeTitle(callback) {
+    setTimeout(() => {
+      callback();
+    }, 2000);
+  }
+
+  constructor() {
+    // this.changeTitle(this.setTitle);
+    this.onComplete().then(this.setTitle);
+    this.title$.subscribe(this.setTitle);
+  }
+
+  private onComplete() {
+    return new Promise((resolve, reject) => {
+      // setTimeout(() => {
+      //   resolve();
+      // }, 2000);
+      setInterval(() => {
+        resolve();
+      }, 2000);
+    });
+  }
+
+  title$ = new Observable(observer => {
+    setInterval(() => {
+      observer.next();
+    }, 2000);
+  });
 
 }
