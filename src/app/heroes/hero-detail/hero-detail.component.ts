2fbb60ad8a5a91ab5c3e1ade20b7b15f5e5138c5
manhtq <manhtq@sunshinegroup.vn>
2022-03-19 08:12:20 +0700

    sp

--- src/app/heroes/hero-detail/hero-detail.component.ts
+++ src/app/heroes/hero-detail/hero-detail.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from 'src/app/model/hero.model';
+import { HeroDetailService } from './hero-detail.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
+  providers: [HeroDetailService]
 })
 export class HeroDetailComponent implements OnInit {
 
   @Input() id: number;
 
-  constructor() { }
+  constructor(private heroDetaiService: HeroDetailService) { }
 
+  private hero: Hero;
   ngOnInit(): void {
+    this.hero = this.heroDetaiService.getHero(this.id);
   }
 
 }
---
+++ src/app/heroes/hero-detail/hero-detail.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HeroService } from 'src/services/heroes/hero.service';
+import { Hero } from 'src/app/model/hero.model';
+
+@Injectable()
+export class HeroDetailService {
+
+  constructor(private heroService: HeroService) { }
+
+  private hero: Hero;
+
+  getHero(id: number) {
+    let heroes = this.heroService.getHeroes();
+    if (!this.hero) {
+      this.hero = heroes.find(hero => hero.id === id);
+    }
+    return this.hero;
+  }
+}
