e302c6c2a1a84f092b32472705331f817ab187d9
manhtq <manhtq@sunshinegroup.vn>
2022-03-21 12:03:30 +0700

    sp

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroListComponent } from './hero-list/hero-list.component';
-import { FavoriteHeroesComponent } from './favorite-heroes/favorite-heroes.component';
+import { FavoriteHeroesComponent } from './favorite-heroes2/favorite-heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
