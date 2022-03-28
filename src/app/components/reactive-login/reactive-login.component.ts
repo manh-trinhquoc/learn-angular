2f5e946b931f1459a5a93ea3b22729343a80d684
manhtq <manhtq@sunshinegroup.vn>
2022-03-28 10:43:03 +0700

    270

--- src/app/components/reactive-login/reactive-login.component.ts
+++ src/app/components/reactive-login/reactive-login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-login',
@@ -8,8 +8,13 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ReactiveLoginComponent implements OnInit {
 
-  username: string;
-  password: string;
+
+  get username(): AbstractControl {
+    return this.loginForm.controls.username;
+  }
+  get password(): AbstractControl {
+    return this.loginForm.controls.password;
+  }
 
   login(){
     const controls = this.loginForm.controls;
@@ -19,13 +24,26 @@ export class ReactiveLoginComponent implements OnInit {
   }
 
   loginForm = new FormGroup({
-    username: new FormControl('tét'),
-    password: new FormControl('')
+    username: new FormControl('test', Validators.required),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ])
   });
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  heroDetails = new FormGroup({
+    name: new FormControl(''),
+    realName: new FormControl(''),
+    biometricData: new FormGroup({
+      age: new FormControl(''),
+      eyes: new FormControl(''),
+      hair: new FormControl('')
+    })
+  })
 
 }
