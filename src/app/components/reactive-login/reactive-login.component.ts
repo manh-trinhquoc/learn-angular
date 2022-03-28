cc26d8c8976ba471d999bd1571ac8922a546f0aa
manhtq <manhtq@sunshinegroup.vn>
2022-03-28 14:34:38 +0700

    277

--- src/app/components/reactive-login/reactive-login.component.ts
+++ src/app/components/reactive-login/reactive-login.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { FormArray } from '@angular/forms';
 
-import { reservedNameValidator } from 'src/app/directives/reserved-name.directive';
 
 @Component({
   selector: 'app-reactive-login',
@@ -10,21 +8,6 @@ import { reservedNameValidator } from 'src/app/directives/reserved-name.directiv
   styleUrls: ['./reactive-login.component.css']
 })
 export class ReactiveLoginComponent implements OnInit {
-
-
-  get username(): AbstractControl {
-    return this.loginForm.controls.username;
-  }
-  get password(): AbstractControl {
-    return this.loginForm.controls.password;
-  }
-
-  login(){
-    const controls = this.loginForm.controls;
-    console.log('User:' + controls.username.value);
-    console.log('Password:' + controls.password.value);
-  
-  }
 
   loginForm = new FormGroup({
     username: new FormControl('test', Validators.required),
@@ -34,8 +17,23 @@ export class ReactiveLoginComponent implements OnInit {
     ])
   });
 
-  constructor(private builder: FormBuilder ) { 
+  get username(): AbstractControl {
+    return this.loginForm.controls.username;
+  }
+  get password(): AbstractControl {
+    return this.loginForm.controls.password;
+  }
 
+  constructor(private builder: FormBuilder ) {   }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    const controls = this.loginForm.controls;
+    console.log('User:' + controls.username.value);
+    console.log('Password:' + controls.password.value);
+  
   }
 
   private buildForm() {
@@ -46,28 +44,6 @@ export class ReactiveLoginComponent implements OnInit {
         Validators.minLength(6)
       ]]
     });
-  }
-
-  ngOnInit(): void {
-  }
-
-  heroDetails = new FormGroup({
-    name: new FormControl('', reservedNameValidator()),
-    realName: new FormControl(''),
-    biometricData: new FormGroup({
-      age: new FormControl(''),
-      eyes: new FormControl(''),
-      hair: new FormControl('')
-    }),
-    powers: new FormArray([])
-  })
-
-  get powers(): FormArray {
-    return this.heroDetails.controls.powers as FormArray;
-  }
-
-  addPower() {
-    this.powers.push(new FormControl(''));
   }
 
 }
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
+
+import { reservedNameValidator } from 'src/app/directives/reserved-name.directive';
+
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
@@ -7,11 +12,52 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class HeroComponent implements OnInit {
 
+  get powers(): FormArray {
+    return this.heroDetails.controls.powers as FormArray;
+  }
+
+  heroDetails = new FormGroup({
+    name: new FormControl('', reservedNameValidator()),
+    realName: new FormControl(''),
+    biometricData: new FormGroup({
+      age: new FormControl(''),
+      eyes: new FormControl(''),
+      hair: new FormControl('')
+    }),
+    powers: new FormArray([])
+  })
+
   @Input() name: string;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addPower() {
+    this.powers.push(new FormControl(''));
+  }
+
+  addHero() {
+    this.heroDetails.setValue({
+      name: 'Maleward',
+      realName: 'Agavens Jenmar',
+      biometricData: {
+        age: 30,
+        eyes: '#006400',
+        hair: '#8b4513'
+      },
+      powers: []
+    });
+  }
+
+  addBio() {
+    this.heroDetails.patchValue({
+      biometricData: {
+        age: 35,
+        hair: '#ff0000'
+      },
+    })
   }
 
 }
